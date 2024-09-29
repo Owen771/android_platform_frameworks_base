4f7835880d1a9d48d57e5b70a008947f1463f667
Owen771 <66254587+Owen771@users.noreply.github.com>
2024-09-29 14:27:30 -0400

    reading zygote

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -846,12 +846,15 @@ public class ZygoteInit {
                 // {@code r == null} in the parent (zygote) process, and {@code r != null} in the
                 // child (system_server) process.
                 if (r != null) {
+                     /* child process: SystemServer (=ZygoteServer?) run in another thread here and return  */ 
                     r.run();
                     return;
                 }
             }
 
             Log.i(TAG, "Accepting command socket connections");
+            
+            /* Parent process: Zygote run a inf loop to listen socket (like req from other component) */
 
             // The select loop returns early in the child process after a fork and
             // loops forever in the zygote.
